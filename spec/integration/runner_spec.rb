0d8c00d1106c10e9927b9cb197f9931ac49d54d3
Shaban Karumba <shabankarumba@gmail.com>
2024-11-16 19:39:14 +0100

    When the incorrect options are entered into the runner rescue and return a message

--- spec/integration/runner_spec.rb
+++ spec/integration/runner_spec.rb
@@ -2,29 +2,38 @@
 
 require 'spec_helper'
 
-RSpec.describe do
-  context 'when the file can be opened' do
-    it 'outputs the robot coordinates' do
-      output = <<~OUTPUT
-        0,1,NORTH
-        0,0,WEST
-        3,3,NORTH
-      OUTPUT
-      expect(`./runner -f test.txt`).to eq(output)
+RSpec.describe 'Runner' do
+  context 'when the options and arguments provided are valid' do
+    context 'when the file can be opened' do
+      it 'outputs the robot coordinates' do
+        output = <<~OUTPUT
+          0,1,NORTH
+          0,0,WEST
+          3,3,NORTH
+        OUTPUT
+        expect(`./runner -f test.txt`).to eq(output)
+      end
+    end
+    context 'when the file cannot be opened' do
+      context 'when the file does exist' do
+        it 'outputs an error message' do
+          expect(`./runner -f file.tx`).to eq("Please enter a file which exists\n")
+        end
+      end
+
+      context 'when the file is not a text file' do
+        it 'outputs an error message' do
+          expect(`./runner -f sample.csv`).to eq("Please only use a text file\n")
+        end
+      end
     end
   end
 
-  context 'when the file cannot be opened' do
-    context 'when the file does exist' do
-      it 'outputs an error message' do
-        expect(`./runner -f file.tx`).to eq("Please enter a file which exists\n")
-      end
-    end
-
-    context 'when the file is not a text file' do
-      it 'outputs an error message' do
-        expect(`./runner -f sample.csv`).to eq("Please only use a text file\n")
-      end
+  context 'when the options and arguments provided are not valid' do
+    it 'outputs an error message' do
+      expect(`./runner -d`).to eq(
+        "Please check the arguments and options you inputted\n"
+      )
     end
   end
 end
